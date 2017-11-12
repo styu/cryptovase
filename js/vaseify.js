1e3b883f7cfc6f6f35007df879bcc6eea8a0bf3f
poofytoo <victorhung92@gmail.com>
2017-11-11 21:53:04 -0500

    vase

--- js/vaseify.js
+++ js/vaseify.js
@@ -1,22 +1,64 @@
 getVaseParams = function (seed) {
+    var seed = seed || [Math.random(), Math.random(), Math.random(), Math.random(), Math.random(), Math.random(), Math.random(), Math.random()]
     var features = {
-        foot1: [2, 0],
-        foot2: [1.5, 1],
-        foot3: [1.5, 1.5],
-        body1: [2.5, 3],
-        body2: [3, 6],
-        body3: [2.7, 8],
-        shoulder1: [1.5, 10],
-        neck1: [1.5, 10.5],
-        neck2: [1.5, 11],
-        mouth1: [3, 13],
-        mouth2: [3, 13.5]
+        foot1: {
+            coords: [2, 0],
+            hashId: 0,
+        },
+        foot2: {
+            coords: [1.5, 1],
+            hashId: 0
+        },
+        foot3: {
+            coords: [1.5, 1.5],
+            hashId: 0
+        },
+        body1: {
+            coords: [2.5, 3],
+            hashId: 1
+        },
+        body2: {
+            coords: [3, 6],
+            hashId: 2
+        },
+        body3: {
+            coords: [2.7, 8],
+            hashId: 3
+        },
+        shoulder1: {
+            coords: [1.5, 10],
+            hashId: 4
+        },
+        neck1: {
+            coords: [1.5, 10.5],
+            hashId: 5
+        },
+        neck2: {
+            coords: [1.5, 11],
+            hashId: 6
+        },
+        mouth1: {
+            coords: [2.5, 13],
+            hashId: 7
+        },
+        mouth2: {
+            coords: [2.5, 13.5],
+            hashId: 7
+        }
     }
 
     var points = [];
-
+    var prevFeature;
     for (i in features) {
-        points.push(features[i])
+        var heightVariance = 0;
+        if (prevFeature) {
+            // get difference between features
+            heightTolerance = features[i].coords[1] - features[prevFeature].coords[1];
+            console.log(heightTolerance);
+            heightVariance = heightTolerance*Math.random()/2
+        }
+        points.push([features[i].coords[0] + seed[features[i].hashId], features[i].coords[1] - heightVariance ])
+        prevFeature = i;
     }
 
     return points
@@ -24,7 +66,7 @@ getVaseParams = function (seed) {
 
 // takes list of points and generates them
 drawVase = function (seed) {
-    
+
     var params = getVaseParams(seed);
     var scale = 30;
     
@@ -35,7 +77,7 @@ drawVase = function (seed) {
 
     for (i in params) {
         vertex = params[i]
-        c2.lineTo(vertex[0]*scale + Math.random()*60-30, (13.5-vertex[1])*scale);
+        c2.lineTo(vertex[0]*scale, (13.5-vertex[1])*scale);
     }
     c2.lineTo(0, 0)
     c2.closePath();
@@ -43,60 +85,7 @@ drawVase = function (seed) {
 }
 
 $(function () {
+    // hash = [Math.random(), Math.random(), Math.random(), Math.random(), Math.random(), Math.random(), Math.random()]
+    hash = undefined;
     drawVase(hash);
 });
-
-
-/*
-
-$(document).ready(function () {
-    var marker = 'rgba(9,120,120,0.9)';
-    var markerWidth = 1;
-
-    var lastEvent;
-    var mouseDown = false;
-
-    var $canvas = $('canvas');
-    var context = $('canvas')[0].getContext('2d');
-    var $canvas = $('#canvas');
-    //console.log($canvas);
-
-    $canvas.mousedown(function (e) {
-        lastEvent = e;
-        mouseDown = true;
-        //console.log(lastEvent);
-    }).mousemove(function (e) {
-        if (mouseDown) {
-
-            context.beginPath();
-
-            context.moveTo(lastEvent.offsetX, lastEvent.offsetY);
-            context.lineTo(e.offsetX, e.offsetY);
-            context.lineWidth = markerWidth;
-            context.strokeStyle = marker;
-            context.lineCap = 'round';
-            context.stroke();
-
-            lastEvent = e;
-
-        }
-    }).mouseup(function () {
-        mouseDown = false;
-    });
-
-    var changeWidth = function () {
-        markerWidth = $('#marker').val();
-        console.log(markerWidth);
-    }
-
-    var clear = function () {
-        context.clearRect(0, 0, 450, 500);
-    }
-
-    $('#clear').on('click', clear);
-
-    $('#marker').change(changeWidth);
-
-});//endJQuery///////
-
-*/
